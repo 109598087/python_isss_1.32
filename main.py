6fd5b1edff5612f2224032e09ed3692609d0313b
t109598087@ntut.org.tw <t109598087@ntut.org.tw>
2021-09-22 23:52:52 +0800

    ok

--- main.py
+++ main.py
@@ -1,24 +1,38 @@
 import math
 
-def get_max_circles_in_rectangle(short_length, long_length):
-    # triangle_pattern_part
-    triangle_pattern_t = pow(3, 1 / 2) / 2
-    other_row = math.floor(short_length / triangle_pattern_t * (1 - triangle_pattern_t))
-    total_triangle_pattern_row = math.floor(other_row / (1 - triangle_pattern_t))
 
-    total_circles = long_length + 1
-    for i in range(total_triangle_pattern_row):
-        if i % 2 == 0:
-            total_circles += long_length
-        else:
+def get_max_circles_in_rectangle2(short_length, long_length):
+    triangle_pattern_t = pow(3, 1 / 2) / 2
+    max_triangle_row = math.floor(short_length / triangle_pattern_t)
+    # print(max_triangle_row)
+    all_list = list()
+    for triangle_pattern_row in range(max_triangle_row + 1):
+
+        total_circles = long_length + 1
+        for i in range(triangle_pattern_row):
+            if i % 2 == 0:
+                total_circles += long_length
+            else:
+                total_circles += (long_length + 1)
+        # rectangle_pattern_part
+        remain_length = math.floor(short_length - triangle_pattern_row * triangle_pattern_t - 1)
+        for i in range(remain_length + 1):
             total_circles += (long_length + 1)
 
-    # rectangle_pattern_part
-    remain_length = math.floor(short_length - total_triangle_pattern_row * triangle_pattern_t)
-    for i in range(remain_length + 1):
-        total_circles += (long_length + 1)
-    return total_circles
+        all_list.append(total_circles)
+    return max(all_list)
 
 
-print(get_max_circles_in_rectangle(8, 9))
-print(get_max_circles_in_rectangle(10, 10))
+def result_answer(short_length, long_length):
+    a = get_max_circles_in_rectangle2(short_length, long_length)
+    b = get_max_circles_in_rectangle2(long_length, short_length)
+    if a >= b:
+        return a
+    else:
+        return b
+
+
+print(result_answer(8, 9))
+print(result_answer(10, 10))
+print(result_answer(61, 67))
+print(result_answer(53, 59))
